95fa0decdaa6a6750cffc1dc1097decf643d03ae
Jean-Baptiste BESNARD <jbbesnard@paratools.fr>
2017-05-04 13:15:53 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,40 @@
+MPI Snippets
+============
+
+This repository contains vim snippets for MPI and their meta-description. 
+These definitions are to be used with a VIM snippet implementations such as
+[ultisnips](github.com/SirVer/ultisnips) or through a bundle such as 
+[vim-snippets](https://github.com/honza/vim-snippets).
+
+Install
+-------
+
+Considering that you use [vim-snippets](https://github.com/honza/vim-snippets),
+copy the mpi.snippets file in the 
+*~/.vim/vim-snippets/UltiSnips* directory, or *~/.vim/bundle/vim-snippets/UltiSnips/*
+if you use bundles.
+
+Then add the following to your ~/.vimrc to load the definition:
+
+```
+autocmd VimEnter * UltiSnipsAddFiletypes mpi
+```
+
+
+Use
+---
+
+Once installed you may try to type MPI_Send and then use your completion key
+(most of the time TAB) to trigger the Snippet. MPI snippers are done
+such as all parametters are successively highlighted to be replaced, acting
+as both a snipped and a cheatcheat of what is where.
+
+The following nonMPI-function-ish snipets are defined:
+
+CW -> MPI_COMM_WORLD
+CS -> MPI_COMM_SELF
+
+mpimain -> A main with INIT and FINALIZE
+
+
+
---
+++ gen.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python 
+import json
+import sys
+import os
+import string
+
+scriptpath = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+with open(scriptpath + '/mpi.json') as data_file:    
+	mpi_interface = json.load(data_file)
+
+
+IFACE=""
+
+for f in mpi_interface:
+	fd = mpi_interface[f]
+	rtype=""
+	if(f=="MPI_Wtime"):
+		rtype = "double"
+	else:
+		rtype = "int"
+	IFACE+= "snippet " + f + "\n"
+	IFACE+= f + "("
+	for i in range(0, len(fd)):
+		arg=fd[i]
+		name = arg[1];
+		ctype = arg[0];
+		#ARRAY CASE
+		array=""
+		try:
+			idx=ctype.index("[")
+		except ValueError:
+			idx=-1
+		if idx!=-1:
+			array=ctype[idx:]
+   			ctype=ctype[0:idx]
+		IFACE+=" ${" + str(i+1) + ":" + ctype + " " + name + array + "} "
+		if i < (len(fd) - 1):
+			IFACE += ","
+
+	IFACE+= ");\n"
+	IFACE+= "endsnippet\n"
+
+#This is the static part
+IFACE += """
+snippet mainmpi
+#include <mpi.h>
+
+int main( int argc, char *argv[])
+{
+	MPI_Init(&argc, &argv);
+
+	${1:}
+
+	MPI_Finalize();
+
+	return 0;
+}
+endsnippet
+
+snippet CS
+MPI_SELF
+endsnippet
+
+snippet CW
+MPI_COMM_WORLD
+endsnippet
+"""
+
+
+
+f = open("mpi.snippets", "w" )
+
+f.write( IFACE );
+
+f.close()
+
+
