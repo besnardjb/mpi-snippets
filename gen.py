39d53f34905591548866b03dbb2fe80c1c4b0110
Julien Adam <adamj@paratools.com>
2019-10-28 14:42:34 +0100

    Update w/ VScode snippet

--- gen.py
+++ gen.py
@@ -49,8 +49,10 @@ snippet mainmpi
 int main( int argc, char *argv[])
 {
 	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	${1:}
+	${1:printf(\"Rank: %d/%d\\n\", rank, size);}
 
 	MPI_Finalize();
 
